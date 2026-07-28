36c3cae6a72544767467a00cfc94b9e896cc964f
Rishab sharma <rehnirishab@gmail.com>
2026-07-28 07:23:06 +0530

    changes connection settings

--- app/database.py
+++ app/database.py
@@ -2,14 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from app.config import DB_URL
 
-DB_URL=DB_URL
-
-engine =create_engine(DB_URL)
+engine = create_engine(
+    DB_URL,
+    pool_pre_ping=True,
+)
 
 SessionLocal = sessionmaker(
-    autocommit = False,
-    autoflush = False,
-    bind= engine
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
 )
 
 class Base(DeclarativeBase):
